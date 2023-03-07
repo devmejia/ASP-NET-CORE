f0b6a186edbd91b06cec04815f15f312f97e46d2
ZEUSTECNOLOGIA\emmejiat <emmejiat@siesa.com>
2023-03-07 10:40:04 -0500

    Creación del Modelo Escuela

---
+++ Models/Escuela.cs
@@ -0,0 +1,9 @@
+namespace asp_net_core.Models
+{
+    public class Escuela
+    {
+        public string EscuelaId { get; set; }
+        public string Nombre { get; set; }
+        public int AñoFundación { get; set; }
+    }
+}
